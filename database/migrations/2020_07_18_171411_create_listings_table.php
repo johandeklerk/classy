b0fde693f643e2b6701b1a3069564c3169a1e395
Johan De Klerk <johandk996@gmail.com>
2020-07-20 10:04:48 +0200

    Add some initial tables - no indexes yet

---
+++ database/migrations/2020_07_18_171411_create_listings_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateListingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('listings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('category_id');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('description');
+            $table->dateTime('date_online')->nullable();
+            $table->dateTime('date_offline')->nullable();
+            $table->double('price', 10, 2);
+            $table->string('currency');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('listing');
+    }
+}
